ec18b4e4f80448d6a835bd9b42d69d4c34c43a45
Theodore Keloglou <zf@sirodoht.com>
2024-05-08 12:39:49 +0100

    add user model

--- main/models.py
+++ main/models.py
@@ -1,1 +1,12 @@
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    first_name = None
+    last_name = None
+    email = models.EmailField(
+        blank=True,
+        null=True,
+        unique=True,
+    )
--- shoshincollege/settings.py
+++ shoshincollege/settings.py
@@ -77,6 +77,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "shoshincollege.wsgi.application"
 
+AUTH_USER_MODEL = "main.User"
+
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases

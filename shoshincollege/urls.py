01d779f5dcd7c465aeafac2786ee6ae20266a635
Theodore Keloglou <zf@sirodoht.com>
2024-05-24 16:24:12 +0100

    change django admin url

--- shoshincollege/urls.py
+++ shoshincollege/urls.py
@@ -19,6 +19,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("dja/", admin.site.urls),
     path("", include("main.urls")),
 ]

200252d3b0f6080cc59c2673cea723f1d05386d2
Theodore Keloglou <zf@sirodoht.com>
2024-10-27 11:06:49 +0000

    Add feedback url

--- main/urls.py
+++ main/urls.py
@@ -3,9 +3,16 @@ from django.views.generic.base import RedirectView
 
 from main import views
 
+FEEDBACK_URL = "https://docs.google.com/forms/d/e/1FAIpQLSeWBKJVxev8e4DwWKaHXynqBEt1hkipnEo6Jgdn99m9p_ymOQ/viewform"
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("classes/", RedirectView.as_view(url="/courses/"), name="classes"),
+    path(
+        "feedback/",
+        RedirectView.as_view(url=FEEDBACK_URL),
+        name="classes",
+    ),
     path("values/", views.values, name="values"),
     path("courses/", views.courses, name="courses"),
     path("code-of-conduct/", views.coc, name="coc"),
